711cf3b87166fde0dc937fc580878a3fc4e6f1c4
Seva Parfenov <vsevolodp@gmail.com>
2022-07-19 23:54:08 -0700

    Configure to use Apple Fn

--- keyboards/kbdfans/kbd75hs/rules.mk
+++ keyboards/kbdfans/kbd75hs/rules.mk
@@ -12,9 +12,10 @@ MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = yes           # Enable N-Key Rollover
+NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
+APPLE_FN_ENABLE = yes       # Enable Apple Fn key functionality
 
 LAYOUTS = 75_ansi
